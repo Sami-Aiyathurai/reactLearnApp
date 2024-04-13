1816c17493dce767ac3152b1723e867a4bc7f5b8
Sami Aiyathurai <53624695+Sami-Aiyathurai@users.noreply.github.com>
2024-04-13 14:37:09 -0400

    first commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,35 @@
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup(props: Props) {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 ? <p>No item Found</p> : null}
+      {items.length === 0 && <p>No Item Found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
